11a612ea399db2cb34971cf631519015f792b00a
Артём Миков <artem.mikov.2003@mail.ru>
2024-10-11 18:18:45 +0500

    Все команды сделаны и отредактированы

--- ExcelAccounting.Loader/Commands/ReportCommand.cs
+++ ExcelAccounting.Loader/Commands/ReportCommand.cs
@@ -14,25 +14,54 @@ namespace ExcelAccounting.Loader.Commands
         public ReportCommand(List<string> args)
         {
             _model = new();
-            if (string.IsNullOrWhiteSpace(args.First()))
+            if (!string.IsNullOrWhiteSpace(args.First()))
             {
                 if (int.TryParse(args.First().Trim(), out var monthNUmber))
                     _model.Date = new DateOnly(DateTime.Now.Year, monthNUmber, 1);
                 else if (DateOnly.TryParse(args.First().Trim(), out var date))
-                    _model.Date = date;
+                    _model.Date = new DateOnly(date.Year,date.Month,1);
                 else
                     throw new ArgumentException(ICommand.ErrorParseDate);
 
             }
-            else _model.Date = new DateOnly(DateTime.Now.Year,DateTime.Now.Month,1);
+            else _model.Date = new DateOnly(DateTime.Now.Year, DateTime.Now.Month, 1);
         }
-        public void Execute(ITelegramBotClient bot, long chatId)
+        public async void Execute(ITelegramBotClient bot, long chatId)
         {
-            
+            var fromDate = _model.Date;
+            var toDate = _model.Date.AddMonths(1);
+            var balance = DataWorker.TransactionModels
+                .Where(x => x.Date < toDate)
+                .Sum(x => x.Value);
+            var data = DataWorker.TransactionModels.Where(x => x.Date >= fromDate && x.Date < toDate);
+            var income = data.Sum(x => x.Value > 0 ? x.Value : 0);
+            var unincome = data.Sum(x => x.Value < 0 ? x.Value : 0);
+            var result = data.Sum(x => x.Value);
+            var categories = data.GroupBy(x=>x.Type).Select(x=>(x.Key,x.Sum(y=>y.Value)));
+            var accounts = data.GroupBy(x => x.Account).Select(x => (x.Key, x.Sum(y => y.Value)));
+            var builder = new StringBuilder();
+            builder.AppendLine($"Отчётный период: {fromDate}-{toDate}");
+            builder.AppendLine($"----------------------------------\n");
+            builder.AppendLine($"Заработано = {income}");
+            builder.AppendLine($"Потрачено = {unincome}");
+            builder.AppendLine($"Разница = {result}");
+            builder.AppendLine($"Баланс да отчётный месяц = {balance}");
+            builder.AppendLine($"\nПо счетам: ");
+            foreach (var a in accounts)
+                builder.AppendLine($"Счёт - {a.Key}, сумма операций = {a.Item2}");
+            builder.AppendLine($"\nПо категориям:");
+            foreach (var c in categories)
+                builder.AppendLine($"Категория - {c.Key}, сумма операций = {c.Item2}");
+            builder.AppendLine($"\nНа текущий момент на сохранении: ");
+            foreach (var s in DataWorker.StashModels)
+                builder.AppendLine($"Хранилище - {s.Name}, сумма = {s.Value}");
+            await SendMessageAsync(bot,chatId,builder.ToString());
+
         }
     }
+   
     public class ReportModel
     {
-        public DateOnly? Date { get; set; }
+        public DateOnly Date { get; set; }
     }
 }
